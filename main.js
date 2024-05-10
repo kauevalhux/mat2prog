afd94f7295819f1aafe7d5bb30648aa9d2fd9fc9
kauevalhux <129780558+kauevalhux@users.noreply.github.com>
2024-05-10 10:42:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
  const tempoObjetivo01 = new Date("2024-10-05T00:00:00");
  const tempoObjetivo02 = new Date("2024-12-05T00:00:00");
  const tempoObjetivo03 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo04 = new Date("2025-02-05T00:00:00");
+ const tempoObjetivo04 = new Date("2025-02-05T00:00:00");
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
 
