5d89b96ac4cc8557168b49565ba5f36757c3c8f8
kauevalhux <129780558+kauevalhux@users.noreply.github.com>
2024-05-10 10:39:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll(".botao");
+ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 
